78c694fa0ec4519e158932902eb338f97dd0dbc3
Sabrina Rocha <delrock876@gmail.com>
2019-09-14 17:04:25 -0700

    html outline

---
+++ js/app.js
@@ -0,0 +1,54 @@
+const questions = [
+{
+  question: 'What pokemon does Pikachu evolve into?',
+  answers: ['Raichu', 'Charmander', 'Bulbasaur', 'Unicorn'],
+ correctAnswer: 'Raichu'
+},
+{
+  question: `What's the most effective pokemon ball in the game?`,
+  answers: ['Great Ball', 'Master Ball', 'Ultra Ball', 'Super Ball'],
+  correctAnswer: 'Master Ball'
+},
+{
+  question: `How many Gym Badges must a trainer collect before challenging the Elite Four?`,
+  answers: ['Six', 'Seven', 'Eight', 'Five'],
+  correctAnswer: 'Eight'
+},
+{
+  question: `What's the device trainers use to keep a record of their Pokemon encounters?`,
+  answers: ['Pokedock','Pokefind','Pokephone', 'Pokedex'],
+  correctAnswer: 'Pokedex'
+},
+{
+  question: `If you need to buy supplies in the Pokemon world, where do you go?`,
+  answers: ['Poke Mart', 'Pokemon Center', 'Gym', 'Costco'],
+  correctAnswer: 'Poke Mart'
+},
+{
+  question: 'What are the three types of starter Pokemon?',
+  answers: [`Psychic, Fighting and Ghost`, `Dragon, Flying and Normal`, `Electric, Ground, Poison`, `Fire, Water, Grass`],
+  correctAnswer: 'Fire, Water, Grass'
+},
+
+{
+  question: `What type of attacks are Normal type Pokemon immune to?`,
+  answers: ['Fighting', 'Ghost', 'Fire', 'Dark'],
+  correctAnswer: 'Ghost'
+},
+{
+  question: `Which of these Legendary Pokemon is known to appear randomly throughout the Johto region?`,
+  answers: ['Registeel', 'Celebi', 'Keldeo', 'Suicune'],
+  correctAnswer: 'Suicune'
+},
+{
+  question: `In generation 1, which Pokemon is known for its lethal punches?`,
+  answers: ['Lickitung', 'Nidoking', 'Hitmonchan', 'Slowbroe'],
+  correctAnswer: 'Suicune'
+},
+{
+  question: `What's this guy's name?`,
+  answers: ['Naruto', 'Ash', 'Greg', 'Brock'],
+  correctAnswer: 'Ash'
+}
+
+]
