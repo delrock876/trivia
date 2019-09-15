8febc763cf2264df45e1ece40059f69f111bc63f
Sabrina Rocha <delrock876@gmail.com>
2019-09-14 22:27:57 -0700

    fixed answers and images

--- js/app.js
+++ js/app.js
@@ -6,55 +6,55 @@ let currentCard = 0
 //ARRAY OF POKEMON OBJECTS
 const cards = [
   {
-    image: "assets/images/pokemon1.png",
+    image: "assets/images/pokemon0.png",
     answers: ['Eevee', 'Vulpix', 'Bulbasaur', 'Deer'],
     correctAnswer: 'Eevee'
   },
   {
-    image: "assets/images/pokemon2.png",
+    image: "assets/images/pokemon1.png",
     answers: ['Charizard', 'Torchic', 'Charmander', 'Fire Lizard'],
     correctAnswer: 'Charmander'
   },
   {
-    image: "assets/images/pokemon3.png",
+    image: "assets/images/pokemon2.png",
     answers: ['Advanced Fire Lizard', 'Flareon', 'Entei', 'Charizard'],
     correctAnswer: 'Charizard'
   },
   {
+    image: "assets/images/pokemon3.png",
+    answers: ['Mew', 'Mewtwo', 'Meowth', 'Meowie'],
+    correctAnswer: 'Mewtwo'
+  },
+  {
     image: "assets/images/pokemon4.png",
-    answers: ['Pokedock', 'Pokefind', 'Pokephone', 'Pokedex'],
-    correctAnswer: 'Pokedex'
+    answers: ['Psychic', 'Golduck', 'Psyduck', 'Slowpoke'],
+    correctAnswer: 'Psyduck'
   },
   {
     image: "assets/images/pokemon5.png",
-    answers: ['Poke Mart', 'Pokemon Center', 'Gym', 'Costco'],
-    correctAnswer: 'Poke Mart'
-  },
-  {
-    image: "assets/images/pokemon6.png",
-    answers: [`Psychic, Fighting and Ghost`, `Dragon, Flying and Normal`, `Electric, Ground, Poison`, `Fire, Water, Grass`],
-    correctAnswer: 'Fire, Water, Grass'
+    answers: [`Jigglypuff`, `Snorlax`, `Snorbro`, `Munchlax`],
+    correctAnswer: 'Snorlax'
   },
 
   {
+    image: "assets/images/pokemon6.png",
+    answers: ['Lapras', 'JigglyPuff', 'Chansey', 'Clefable'],
+    correctAnswer: 'JigglyPuff'
+  },
+  {
     image: "assets/images/pokemon7.png",
-    answers: ['Fighting', 'Ghost', 'Fire', 'Dark'],
-    correctAnswer: 'Ghost'
+    answers: ['Arbok', 'Seviper', 'Dragonair', 'Ekans'],
+    correctAnswer: 'Ekans'
   },
   {
     image: "assets/images/pokemon8.png",
-    answers: ['Registeel', 'Celebi', 'Keldeo', 'Suicune'],
-    correctAnswer: 'Suicune'
+    answers: ['Gloom', 'Oddish', 'Bellsprout', 'Muk'],
+    correctAnswer: 'Gloom'
   },
   {
     image: "assets/images/pokemon9.png",
-    answers: ['Lickitung', 'Nidoking', 'Hitmonchan', 'Slowbroe'],
-    correctAnswer: 'Suicune'
-  },
-  {
-    image: "assets/images/pokemon10.png",
-    answers: ['Naruto', 'Ash', 'Greg', 'Brock'],
-    correctAnswer: 'Ash'
+    answers: ['Kadabra', 'Slowpoke', 'Abra', 'Machop'],
+    correctAnswer: 'Abra'
   }
 ]
 //END ARRAY
@@ -62,30 +62,39 @@ const cards = [
 
 // DISPLAYS OBJECT INFO ON CARD
 const assignCard = () => {
+
+  //displays image
   document.getElementById('pokeImage').src = cards[currentCard].image
+
+//displays answers on buttons
   for (let i = 0; i < cards[currentCard].answers.length; i++) {
-    document.getElementById(`ans${i + 1}`).innerHTML = cards[currentCard].answers[i].valueOf()
+    document.getElementById(`ans${i + 1}`).innerHTML = cards[currentCard].answers[i]
   }
 }
 assignCard()
+
 
 //CHECKS YOUR ANSWER AGAINST CORRECT ANSWER
 document.addEventListener('click', event => {
   
   if (event.target.classList.contains("ans")) {
     yourAns = event.target.innerHTML
+
+    if(currentCard < 10){
     if (yourAns === cards[currentCard].correctAnswer) {
       alert("you're right!")
       console.log(currentCard++)
-      score++
       assignCard()
+      score++
     } else if (yourAns !== cards[currentCard].correctAnswer) {
       alert('nope!')
       console.log(currentCard++)
       assignCard()
-    }else if (currentCard === cards.length-1){
-      alert('you finished')
     }
+
+  }else if (currentCard === 10){
+    alert('done')
+  }
   }
 
 })
