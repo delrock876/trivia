af52ee7f5f1b6253b7ea5ea87681d48aaee2ebf4
Sabrina Rocha <delrock876@gmail.com>
2019-09-14 18:10:41 -0700

    yourAnswer assigned onclick event

--- js/app.js
+++ js/app.js
@@ -1,54 +1,79 @@
-const questions = [
+let correct = ""
+let yourAns = ""
+
+//ARRAY OF POKEMON OBJECTS
+const cards = [
 {
-  question: 'What pokemon does Pikachu evolve into?',
+  num: 1,
+  image: "assets/images/pokemon1.png",
   answers: ['Raichu', 'Charmander', 'Bulbasaur', 'Unicorn'],
  correctAnswer: 'Raichu'
 },
 {
-  question: `What's the most effective pokemon ball in the game?`,
+  num: 2,
+  image:"assets/images/pokemon2.png",
   answers: ['Great Ball', 'Master Ball', 'Ultra Ball', 'Super Ball'],
   correctAnswer: 'Master Ball'
 },
 {
-  question: `How many Gym Badges must a trainer collect before challenging the Elite Four?`,
+  num: 3,
+  image: "assets/images/pokemon3.png",
   answers: ['Six', 'Seven', 'Eight', 'Five'],
   correctAnswer: 'Eight'
 },
 {
-  question: `What's the device trainers use to keep a record of their Pokemon encounters?`,
+  num: 4,
+  image:"assets/images/pokemon4.png",
   answers: ['Pokedock','Pokefind','Pokephone', 'Pokedex'],
   correctAnswer: 'Pokedex'
 },
 {
-  question: `If you need to buy supplies in the Pokemon world, where do you go?`,
+  num: 5,
+  image: "assets/images/pokemon5.png",
   answers: ['Poke Mart', 'Pokemon Center', 'Gym', 'Costco'],
   correctAnswer: 'Poke Mart'
 },
 {
-  question: 'What are the three types of starter Pokemon?',
+  num: 6,
+  image: "assets/images/pokemon6.png",
   answers: [`Psychic, Fighting and Ghost`, `Dragon, Flying and Normal`, `Electric, Ground, Poison`, `Fire, Water, Grass`],
   correctAnswer: 'Fire, Water, Grass'
 },
 
 {
-  question: `What type of attacks are Normal type Pokemon immune to?`,
+  num: 7,
+  image: "assets/images/pokemon7.png",
   answers: ['Fighting', 'Ghost', 'Fire', 'Dark'],
   correctAnswer: 'Ghost'
 },
 {
-  question: `Which of these Legendary Pokemon is known to appear randomly throughout the Johto region?`,
+  num: 8,
+  image: "assets/images/pokemon8.png",
   answers: ['Registeel', 'Celebi', 'Keldeo', 'Suicune'],
   correctAnswer: 'Suicune'
 },
 {
-  question: `In generation 1, which Pokemon is known for its lethal punches?`,
+  num: 9,
+  image: "assets/images/pokemon9.png",
   answers: ['Lickitung', 'Nidoking', 'Hitmonchan', 'Slowbroe'],
   correctAnswer: 'Suicune'
 },
 {
-  question: `What's this guy's name?`,
+  num: 10,
+  image: "assets/images/pokemon10.png",
   answers: ['Naruto', 'Ash', 'Greg', 'Brock'],
   correctAnswer: 'Ash'
 }
-
 ]
+//END ARRAY
+
+
+document.addEventListener('click', function(event){
+  if(event.target.id === "ans"){
+yourAns = document.getElementById(event.target.).innerHTML
+
+})
+
+// for(let i = 0; i< cards.length; i++){
+// console.log(cards)
+// }
